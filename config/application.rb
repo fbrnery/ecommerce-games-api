75045e801b482e91ac03c1e86257b6eca6355778
Fabricio <fbrnery@gmail.com>
2023-08-27 12:13:23 -0300

    Alteração do idioma para Português

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module EcommerceGamesApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+ 
+    # I18n config
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
 
     # Configuration for the application, engines, and railties goes here.
     #
