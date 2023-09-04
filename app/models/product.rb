a241faa2b4565b94da59431494ce0ec15a15f218
Fabricio <fbrnery@gmail.com>
2023-09-04 20:24:56 -0300

    Adiçao de imagem para Produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
 
+  has_many :categories, through: :product_categories
+
+  has_one_attached :image
+  validates :image, presence: true
 end
